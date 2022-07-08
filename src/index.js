bdfa022c4b4cf2918d9294c114823010ddb4c442
Utkarshini12 <arorautkarshini@gmail.com>
2022-07-08 23:03:01 +0530

    oauth

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+  domain="dev-zrdj523t.us.auth0.com"
+  clientId="V7dv0Fy1TsQieSo4fv34UEBfwoYp1sLf"
+  redirectUri={window.location.origin}
+>
+    <App /></Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
